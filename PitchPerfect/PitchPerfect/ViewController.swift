b2c7b48f135ae6bdaa2a2e87b3fc9b2f1f2e7108
Jordan <jtm245@nau.edu>
2016-09-21 11:27:26 -0500

    AutoLayout complete

--- PitchPerfect/PitchPerfect/ViewController.swift
+++ PitchPerfect/PitchPerfect/ViewController.swift
@@ -9,6 +9,8 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var recordingLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +22,13 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func recordAudio(_ sender: AnyObject) {
+        print("record button pressed!")
+        recordingLabel.text = "Recording in progress"
+    }
 
+    @IBAction func stopRecording(_ sender: AnyObject) {
+        print("stop recording button pressed")
+    }
 }
 
