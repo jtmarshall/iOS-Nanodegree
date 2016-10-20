7321adec0d2132ce8646ae0833299a808f13b8a5
Jordan <jtm245@nau.edu>
2016-10-20 14:40:44 -0500

    added button and label

--- MemeMe/Click Counter/Click Counter/ViewController.swift
+++ MemeMe/Click Counter/Click Counter/ViewController.swift
@@ -12,11 +12,20 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        //My Label
         var label = UILabel()
-        label.frame = CGRect(x: 150,y: 150,width: 60,height: 60)
+        label.frame = CGRect(x: 150, y: 150, width: 60, height: 60)
         label.text = "0"
+        
+        self.view.addSubview(label)
+        
+        //Button
+        var button = UIButton()
+        button.frame = CGRect(x: 150, y: 250, width: 60, height: 60)
+        button.setTitle("Click", for: .normal)
+        button.setTitleColor(UIColor.blue, for: .normal)
+        self.view.addSubview(button)
     }
-
-    
 }
 
