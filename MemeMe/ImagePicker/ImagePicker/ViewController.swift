d3c571ea92b6c41df1a6439668e97618784cf5d0
Jordan <jtm245@nau.edu>
2016-10-21 09:57:49 -0500

    finished image picker

--- MemeMe/ImagePicker/ImagePicker/ViewController.swift
+++ MemeMe/ImagePicker/ImagePicker/ViewController.swift
@@ -15,8 +15,26 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    //image picker controller
     @IBAction func experiment() {
         let nextController = UIImagePickerController()
+        self.present(nextController, animated: true, completion: nil)
+    }
+    
+    //activity view controller
+    @IBAction func activityViewExperiment() {
+        let image = UIImage()
+        let nextController = UIActivityViewController(activityItems: [image], applicationActivities: nil)
+        self.present(nextController, animated: true, completion: nil)
+    }
+    
+    //alert view controller
+    @IBAction func alertViewExperiment() {
+        let nextController = UIAlertController()
+        let okAction = UIAlertAction(title: "ok", style: UIAlertActionStyle.default) { action in
+            self.dismiss(animated: true, completion: nil)
+        }
+        nextController.addAction(okAction)
         self.present(nextController, animated: true, completion: nil)
     }
 
