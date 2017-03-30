9463c373ff817ae5bc518ff68c5988be5a62b497
Jordan <jtm245@nau.edu>
2017-03-30 10:58:16 -0500

    finished serialization for json

--- sleeping-in-library/SleepingInTheLibrary/SleepingInTheLibrary/ViewController.swift
+++ sleeping-in-library/SleepingInTheLibrary/SleepingInTheLibrary/ViewController.swift
@@ -42,9 +42,53 @@ class ViewController: UIViewController {
     
     private func getImageFromFlickr() {
         
-        let url = URL(string: "\(Constants.Flickr.APIBaseURL)?\(Constants.FlickrParameterKeys.Method)=\(Constants.FlickrParameterValues.GalleryPhotosMethod)&\(Constants.FlickrParameterKeys.APIKey)=\(Constants.FlickrParameterValues.APIKey)&\(Constants.FlickrParameterKeys.GalleryID)=\(Constants.FlickrParameterValues.GalleryID)&\(Constants.FlickrParameterKeys.Extras)=\(Constants.FlickrParameterValues.MediumURL)&\(Constants.FlickrParameterKeys.Format)=\(Constants.FlickrParameterValues.ResponseFormat)&\(Constants.FlickrParameterKeys.NoJSONCallback)=\(Constants.FlickrParameterValues.DisableJSONCallback)")!
+        let methodParameters = [
+            Constants.FlickrParameterKeys.Method: Constants.FlickrParameterValues.GalleryPhotosMethod,
+            Constants.FlickrParameterKeys.APIKey: Constants.FlickrParameterValues.APIKey,
+            Constants.FlickrParameterKeys.GalleryID: Constants.FlickrParameterValues.GalleryID,
+            Constants.FlickrParameterKeys.Extras: Constants.FlickrParameterValues.MediumURL,
+            Constants.FlickrParameterKeys.Format: Constants.FlickrParameterValues.ResponseFormat,
+            Constants.FlickrParameterKeys.NoJSONCallback: Constants.FlickrParameterValues.DisableJSONCallback
+        ]
         
-        print(url)
+        let session = URLSession.shared
+        let urlString = Constants.Flickr.APIBaseURL + escapedParameters(methodParameters as [String:AnyObject])
+        let url = URL(string: urlString)!
+        let request = URLRequest(url: url)
+        
+        let task = session.dataTask(with: request) { (data, response, error) in
+            
+            // if an error occurs, print it and re-enable the UI
+            func displayError(_ error: String) {
+                print(error)
+                print("URL at time of error: \(url)")
+                performUIUpdatesOnMain {
+                    self.setUIEnabled(true)
+                }
+            }
+            
+            // no error, woohoo!
+            if error == nil {
+                
+                // there was data returned
+                if let data = data {
+                    
+                    let parsedResult: [String:AnyObject]!
+                    do {
+                        parsedResult = try JSONSerialization.jsonObject(with: data, options: .allowFragments) as! [String:AnyObject]
+                    } catch {
+                        displayError("Could not parse the data as JSON: '\(data)'")
+                        return
+                    }
+                    
+                    print(parsedResult)
+                }
+            }
+        }
+        
+        // start the task!
+        task.resume()
+        
     }
     
     private func escapedParameters(_ parameters: [String:AnyObject]) -> String {
