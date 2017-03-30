c540badf64cac9ff1ded01c12bc43d552e398b75
Jordan <jtm245@nau.edu>
2017-03-30 11:33:32 -0500

    finished sleeping in library app, can now retrieve and display images from flickr api

--- extraExcercises/SleepingInTheLibrary/SleepingInTheLibrary/ViewController.swift
+++ extraExcercises/SleepingInTheLibrary/SleepingInTheLibrary/ViewController.swift
@@ -81,9 +81,22 @@ class ViewController: UIViewController {
                         return
                     }
                     
-                    if let photosDictionary = parsedResult[Constants.FlickrResponseKeys.Photos] as? [String:AnyObject],
-                        let photoArray = photosDictionary[Constants.FlickrResponseKeys.Photo] as? [[String:AnyObject]] {
-                        print(photoArray[0])
+                    if let photosDictionary = parsedResult[Constants.FlickrResponseKeys.Photos] as? [String:AnyObject], let photoArray = photosDictionary[Constants.FlickrResponseKeys.Photo] as? [[String:AnyObject]] {
+                        
+                        let randomPhotoIndex = Int(arc4random_uniform(UInt32(photoArray.count)))
+                        let photoDictionary = photoArray[randomPhotoIndex] as [String:AnyObject]
+                        
+                        if let imageUrlString = photoDictionary[Constants.FlickrResponseKeys.MediumURL] as? String, let photoTitle = photoDictionary[Constants.FlickrResponseKeys.Title] as? String {
+                            
+                            let imageURL = URL(string: imageUrlString)
+                            if let imageData = try? Data(contentsOf: imageURL!) {
+                                performUIUpdatesOnMain {
+                                    self.photoImageView.image = UIImage(data: imageData)
+                                    self.photoTitleLabel.text = photoTitle
+                                    self.setUIEnabled(true)
+                                }
+                            }
+                        } 
                     }
                 }
             }
