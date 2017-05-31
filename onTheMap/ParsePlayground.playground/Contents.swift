577ccfa9d4738708c5a0db5e43be533dae544a5f
Jordan <jtm245@nau.edu>
2017-05-31 09:34:31 -0500

    OnTheMap setup

--- onTheMap/ParsePlayground.playground/Contents.swift
+++ onTheMap/ParsePlayground.playground/Contents.swift
@@ -4,7 +4,7 @@ import UIKit
 import PlaygroundSupport
 
 // this line tells the Playground to execute indefinitely
-PlaygroundPage.current.needsIndefiniteExecution = true
+//PlaygroundPage.current.needsIndefiniteExecution = true
 
 let urlString = "http://quotes.rest/qod.json?category=inspire"
 let url = URL(string: urlString)
